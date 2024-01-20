91fb69d6290d4ebb84b5bad1f732eb40daa68647
thalytagenaro <genarothalyta@gmail.com>
2024-01-20 13:51:18 -0300

    Adding step to test regression model

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ _steps = [
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-#    "test_regression_model"
+   "test_regression_model"
 ]
 
 
@@ -79,6 +79,7 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
+            # Spliting dataset
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "components", "train_val_test_split"),
                 "main",
@@ -91,14 +92,13 @@ def go(config: DictConfig):
             )
 
         if "train_random_forest" in active_steps:
-
             # NOTE: we need to serialize the random forest configuration into JSON
+
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
+            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest step
 
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
@@ -114,14 +114,16 @@ def go(config: DictConfig):
                 },
             )
 
-
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            # Testing regression model
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest",
+                },
+            )
 
 
 if __name__ == "__main__":
